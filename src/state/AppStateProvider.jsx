ca3eb70a57fc15047f94e6eff251c87c82e1ee32
Cole92 <aleksandar9212@gmail.com>
2026-05-15 09:06:48 +0200

    feat: add app state provider foundation

---
+++ src/state/AppStateProvider.jsx
@@ -0,0 +1,21 @@
+import { useReducer } from "react";
+import { appInitialState } from "./appInitialState";
+import { appReducer } from "./appReducer";
+import { AppStateContext } from "./AppStateContext";
+
+/**
+ * Provides global runtime state for the local-first MVP.
+ *
+ * Runtime note:
+ * This connects the reducer to React. Screens can later read runtime state and
+ * dispatch workout-flow actions through this provider.
+ */
+export function AppStateProvider({ children }) {
+  const [state, dispatch] = useReducer(appReducer, appInitialState);
+
+  return (
+    <AppStateContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AppStateContext.Provider>
+  );
+}
